8dac14eb72d78981099bf856629052d624f00330
allan <allanringeval@gmail.com>
2026-09-11 13:52:48 +0200

    feat(vpc): ajouter la passerelle internet et le routage public exercice 2

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,18 @@ output "numero_poste" {
   description = "Numéro utilisé dans les noms des ressources et le fichier de réponses."
   value       = var.numero_poste
 }
+
+output "internet_gateway_id" {
+  description = "Identifiant de la passerelle internet attachee au VPC."
+  value       = module.vpc.internet_gateway_id
+}
+
+output "public_route_table_id" {
+  description = "Table de routage associee au sous-reseau public."
+  value       = module.vpc.public_route_table_id
+}
+
+output "main_route_table_id" {
+  description = "Table principale encore utilisee implicitement par le sous-reseau prive."
+  value       = module.vpc.main_route_table_id
+}
